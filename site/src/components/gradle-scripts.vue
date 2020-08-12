7c3a7278271ff5144c8bce85b381e47de38ed893
Vincent BA <5869062+v1nc3n4@users.noreply.github.com>
2020-08-12 15:49:28 +0200

    Fixed website code tabs.

--- site/src/components/gradle-scripts.vue
+++ site/src/components/gradle-scripts.vue
@@ -2,10 +2,10 @@
     <div class="mb-2">
         <ul class="nav nav-tabs">
             <li class="nav-item">
-                <nuxt-link class="nav-link" :class="groovyTabClass" to="#" append :event="''" @click.native.prevent="setGroovyTabVisible">Groovy</nuxt-link>
+                <a class="nav-link" :class="groovyTabClass" href="#" @click.prevent="setGroovyTabVisible">Groovy</a>
             </li>
             <li class="nav-item">
-                <nuxt-link class="nav-link" :class="kotlinTabClass" to="#" append :event="''" @click.native.prevent="setKotlinTabVisible">Kotlin</nuxt-link>
+                <a class="nav-link" :class="kotlinTabClass" href="#" @click.prevent="setKotlinTabVisible">Kotlin</a>
             </li>
         </ul>
         <div class="pt-3 pb-1 pl-3 bg-light">
