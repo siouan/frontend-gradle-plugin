217826da903f45e56a28577fa257d9db993ee97f
Vincent BA <5869062+v1nc3n4@users.noreply.github.com>
2020-11-01 10:44:28 +0100

    Fixed examples and version numbers.

--- site/src/pages/index.vue
+++ site/src/pages/index.vue
@@ -6,9 +6,9 @@
                 - Integrated <fgp-nodejs-link />, <fgp-npm-link />, <fgp-yarn-link /> builds
             </small>
             <fgp-image-link
-                href="https://github.com/siouan/frontend-gradle-plugin/releases/tag/v4.0.0-jdk11"
-                src="https://img.shields.io/badge/Latest%20release-4.0.0-blue.svg"
-                alt="Latest release 4.0.0"
+                href="https://github.com/siouan/frontend-gradle-plugin/releases/tag/v4.0.1-jdk11"
+                src="https://img.shields.io/badge/Latest%20release-4.0.1-blue.svg"
+                alt="Latest release 4.0.1"
                 class="ml-1"
             />
         </fgp-main-title>
