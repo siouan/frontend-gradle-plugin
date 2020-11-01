d1e5341c282c7a45f3de16aa3cb4d6898bc6f208
Vincent BA <5869062+v1nc3n4@users.noreply.github.com>
2020-11-01 10:08:26 +0100

    Initialized release 4.0.1.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <h2 align="center">Frontend Gradle plugin - Integrated <a href="https://nodejs.org/" title="Node.js">Node.js</a>, <a href="https://www.npmjs.com/" title="npm">npm</a>, <a href="https://yarnpkg.com/" title="Yarn">Yarn</a> builds</h2> 
 <p align="center">
-    <a href="https://github.com/siouan/frontend-gradle-plugin/releases/tag/v4.0.0-jdk11"><img src="https://img.shields.io/badge/Latest%20release-4.0.0-blue.svg" alt="Latest release 4.0.0"/></a>
+    <a href="https://github.com/siouan/frontend-gradle-plugin/releases/tag/v4.0.1"><img src="https://img.shields.io/badge/Latest%20release-4.0.0-blue.svg" alt="Latest release 4.0.1"/></a>
     <a href="https://opensource.org/licenses/Apache-2.0"><img src="https://img.shields.io/badge/License-Apache%202.0-green.svg" alt="License Apache 2.0"/></a>
     <br/>
     <a href="https://travis-ci.com/siouan/frontend-gradle-plugin"><img src="https://travis-ci.com/siouan/frontend-gradle-plugin.svg?branch=4.0-jdk8" alt="Build status"/></a>
--- build.gradle.kts
+++ build.gradle.kts
@@ -36,13 +36,13 @@ dependencies {
     implementation(gradleApi())
     implementation("org.apache.commons:commons-compress:1.20")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.6.2")
-    testImplementation("org.mockito:mockito-core:3.4.6")
-    testImplementation("org.mockito:mockito-junit-jupiter:3.4.6")
-    testImplementation("org.assertj:assertj-core:3.16.1")
-    testImplementation("com.github.tomakehurst:wiremock-jre8:2.27.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.0")
+    testImplementation("org.mockito:mockito-core:3.6.0")
+    testImplementation("org.mockito:mockito-junit-jupiter:3.6.0")
+    testImplementation("org.assertj:assertj-core:3.18.0")
+    testImplementation("com.github.tomakehurst:wiremock:2.27.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
 }
 
 tasks.named<Wrapper>("wrapper") {
