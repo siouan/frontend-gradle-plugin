a41751cabdef4b444bcf37344ac018f9ee689e7b
Vincent BA <5869062+v1nc3n4@users.noreply.github.com>
2021-03-07 14:04:38 +0100

    Initialized release 5.0.1.

--- site/src/pages/index.vue
+++ site/src/pages/index.vue
@@ -6,9 +6,9 @@
                 - Integrated <fgp-nodejs-link />, <fgp-npm-link />, <fgp-yarn-link /> builds
             </small>
             <fgp-image-link
-                href="https://github.com/siouan/frontend-gradle-plugin/releases/tag/v5.0.0"
-                src="https://img.shields.io/badge/Latest%20release-5.0.0-blue.svg"
-                alt="Latest release 5.0.0"
+                href="https://github.com/siouan/frontend-gradle-plugin/releases/tag/v5.0.1"
+                src="https://img.shields.io/badge/Latest%20release-5.0.1-blue.svg"
+                alt="Latest release 5.0.1"
                 class="ml-1"
             />
         </fgp-main-title>
