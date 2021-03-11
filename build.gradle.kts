6be9e49133f7231dd60283f235acf7eea3b52b1c
Vincent BA <5869062+v1nc3n4@users.noreply.github.com>
2021-03-11 21:27:11 +0100

    Backported fix for #149

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,9 +55,9 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.0")
-    testImplementation("org.mockito:mockito-core:3.6.0")
-    testImplementation("org.mockito:mockito-junit-jupiter:3.6.0")
-    testImplementation("org.assertj:assertj-core:3.18.0")
+    testImplementation("org.mockito:mockito-core:3.7.7")
+    testImplementation("org.mockito:mockito-junit-jupiter:3.7.7")
+    testImplementation("org.assertj:assertj-core:3.18.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
 
     intTestImplementation("com.github.tomakehurst:wiremock:2.27.2")
