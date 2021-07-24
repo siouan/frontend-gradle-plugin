550953c651ccf7cbb83b900bb9b62880d0482f93
Vincent BA <5869062+v1nc3n4@users.noreply.github.com>
2021-07-24 22:14:25 +0200

    Initialized release 5.3.0.

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,15 +51,15 @@ configurations["intTestRuntimeOnly"]
 
 dependencies {
     implementation(gradleApi())
-    implementation("org.apache.httpcomponents.client5:httpclient5:5.0.3")
-    implementation("org.apache.commons:commons-compress:1.20")
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.1")
+    implementation("org.apache.commons:commons-compress:1.21")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.0")
-    testImplementation("org.mockito:mockito-core:3.7.7")
-    testImplementation("org.mockito:mockito-junit-jupiter:3.7.7")
-    testImplementation("org.assertj:assertj-core:3.18.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.7.2")
+    testImplementation("org.mockito:mockito-core:3.11.2")
+    testImplementation("org.mockito:mockito-junit-jupiter:3.11.2")
+    testImplementation("org.assertj:assertj-core:3.20.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
 
     intTestImplementation("com.github.tomakehurst:wiremock:2.27.2")
 }
@@ -123,7 +123,7 @@ sonarqube {
         property("sonar.organization", "siouan")
         property("sonar.projectKey", "siouan_frontend-gradle-plugin")
         property("sonar.projectName", "frontend-gradle-plugin")
-        property("sonar.projectVersion", "5.2.0-jdk11")
+        property("sonar.projectVersion", "5.3.0-jdk11")
 
         property("sonar.links.homepage", "https://github.com/siouan/frontend-gradle-plugin")
         property("sonar.links.ci", "https://travis-ci.com/siouan/frontend-gradle-plugin")
