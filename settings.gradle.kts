4f11fa97d41a4e7c8effa34cc52e027c2a931c27
v1nc3n4 <5869062+v1nc3n4@users.noreply.github.com>
2022-09-09 11:59:14 +0200

    chore(deps): upgrade Gradle wrapper to 7.5.1 and plugin dependencies (#186)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,9 +2,9 @@ val fgpArtifactId: String by extra
 
 pluginManagement {
     plugins {
-        id("com.gradle.enterprise") version "3.7.1"
-        id("com.gradle.plugin-publish") version "0.16.0"
-        id("org.sonarqube") version "3.3"
+        id("com.gradle.enterprise") version "3.10.2"
+        id("com.gradle.plugin-publish") version "0.21.0"
+        id("org.sonarqube") version "3.4.0.2513"
     }
 }
 
