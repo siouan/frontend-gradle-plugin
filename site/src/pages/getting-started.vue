66ead77d88ba3caec1a37447de983e064c5bc742
v1nc3n4 <5869062+v1nc3n4@users.noreply.github.com>
2023-08-23 21:59:52 +0200

    docs: update minimum Gradle release for JDK 11 built [skip ci]

--- site/src/pages/getting-started.vue
+++ site/src/pages/getting-started.vue
@@ -8,7 +8,7 @@
         </p>
         <ul>
             <li><fgp-gradle-plugins-link path="/plugin/org.siouan.frontend-jdk17">JDK 17 build</fgp-gradle-plugins-link>: <fgp-gradle-link /> 7.3+</li>
-            <li><fgp-gradle-plugins-link path="/plugin/org.siouan.frontend-jdk11">JDK 11 build</fgp-gradle-plugins-link>: <fgp-gradle-link /> 6.1+</li>
+            <li><fgp-gradle-plugins-link path="/plugin/org.siouan.frontend-jdk11">JDK 11 build</fgp-gradle-plugins-link>: <fgp-gradle-link /> 6.2+</li>
         </ul>
         <p>
             The plugin is built and tested on Linux, Mac OS, Windows (see the list of build environments used in
