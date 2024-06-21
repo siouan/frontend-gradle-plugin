b143c90a3a4f007b05f0fb9817579e3146708ef5
v1nc3n4 <5869062+v1nc3n4@users.noreply.github.com>
2024-06-21 14:18:58 +0200

    docs: fix sitemap and canonical URLs for SEO [skip ci] (#233)

--- site/src/modules/legacy-routes.ts
+++ site/src/modules/legacy-routes.ts
@@ -2,9 +2,9 @@ import { defineNuxtModule, extendRouteRules } from '@nuxt/kit'
 
 export default defineNuxtModule ({
     setup(options, nuxt) {
-        extendRouteRules('/node-npm-npx-yarn-tasks', {
+        extendRouteRules('/node-npm-npx-yarn-tasks/', {
             redirect: {
-                to: `${nuxt.options.app.baseURL}6/node-corepack-npm-pnpm-yarn-tasks`,
+                to: `${nuxt.options.app.baseURL}6/node-corepack-npm-pnpm-yarn-tasks/`,
                 statusCode: 308
             }
         });
