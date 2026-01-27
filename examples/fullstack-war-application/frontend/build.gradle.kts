c1388f34ecc120a7d9721517a62a3d8e403c3b83
v1nc3n4 <5869062+v1nc3n4@users.noreply.github.com>
2026-01-27 22:58:21 +0100

    chore(deps): upgraded dependencies and plugins

--- examples/fullstack-war-application/frontend/build.gradle.kts
+++ examples/fullstack-war-application/frontend/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.siouan.frontend-jdk21") version "10.0.0"
+    id("org.siouan.frontend-jdk21") version "11.0.0"
 }
 
 frontend {
--- examples/yarn-application-with-pnp-linker/build.gradle.kts
+++ examples/yarn-application-with-pnp-linker/build.gradle.kts
@@ -3,7 +3,7 @@ import kotlin.io.path.Path
 import org.siouan.frontendgradleplugin.infrastructure.gradle.InstallFrontendTask
 
 plugins {
-    id("org.siouan.frontend-jdk21") version "10.0.0"
+    id("org.siouan.frontend-jdk21") version "11.0.0"
 }
 
 frontend {
